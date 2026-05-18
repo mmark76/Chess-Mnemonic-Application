056a706b7ae323b51838ea06b7bbd9355b962373
Markides.Markellos <markellos.markides@gmail.com>
2026-05-18 13:10:07 +0300

    Bump user library importer cache after direct import simplification

--- js/structured-data.js
+++ js/structured-data.js
@@ -73,7 +73,7 @@
 (function () {
   try {
     var script = document.createElement("script");
-    script.src = "js/user-library-batch-import.js?v=20260518-3";
+    script.src = "js/user-library-batch-import.js?v=20260518-4";
     script.defer = true;
     document.head.appendChild(script);
   } catch (e) {
